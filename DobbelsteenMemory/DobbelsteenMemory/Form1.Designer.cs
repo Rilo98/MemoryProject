e9a9fb5173f6eb5d2ed792e2029afd19c6004cf8
Loon1700 <Loon1700@student.nhl.nl>
2017-10-11 21:26:11 +0200

    Versie 0.1 - 2 dobbelstenen die dobbelen, plaaatjes bijgevoegd. Werkt!

---
+++ DobbelsteenMemory/DobbelsteenMemory/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace DobbelsteenMemory
+{
+    partial class Dobbelsteen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gooi_1 = new System.Windows.Forms.Button();
+            this.Speler1 = new System.Windows.Forms.Label();
+            this.Speler2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Gooi_1
+            // 
+            this.Gooi_1.Location = new System.Drawing.Point(225, 300);
+            this.Gooi_1.Name = "Gooi_1";
+            this.Gooi_1.Size = new System.Drawing.Size(75, 23);
+            this.Gooi_1.TabIndex = 0;
+            this.Gooi_1.Text = "Gooi!";
+            this.Gooi_1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.Gooi_1.UseVisualStyleBackColor = true;
+            this.Gooi_1.Click += new System.EventHandler(this.Gooi_1_Click);
+            // 
+            // Speler1
+            // 
+            this.Speler1.AutoSize = true;
+            this.Speler1.Location = new System.Drawing.Point(110, 269);
+            this.Speler1.Name = "Speler1";
+            this.Speler1.Size = new System.Drawing.Size(46, 13);
+            this.Speler1.TabIndex = 5;
+            this.Speler1.Text = "Speler 1";
+            // 
+            // Speler2
+            // 
+            this.Speler2.AutoSize = true;
+            this.Speler2.Location = new System.Drawing.Point(380, 269);
+            this.Speler2.Name = "Speler2";
+            this.Speler2.Size = new System.Drawing.Size(46, 13);
+            this.Speler2.TabIndex = 6;
+            this.Speler2.Text = "Speler 2";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(272, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Dobbelsteen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(534, 411);
+            this.Controls.Add(this.Speler2);
+            this.Controls.Add(this.Speler1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Gooi_1);
+            this.Name = "Dobbelsteen";
+            this.Text = "Dobbelsteen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Gooi_1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label Speler1;
+        private System.Windows.Forms.Label Speler2;
+    }
+}
+
---
+++ DobbelsteenMemory/DobbelsteenMemory/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DobbelsteenMemory
+{
+    public partial class Dobbelsteen : Form
+    {
+        public Dobbelsteen()                                            //Main
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Gooi_1_Click(object sender, EventArgs e)           //Gooi button
+        {
+            dobbelsteen();
+        }
+
+        private void dobbelsteen()                                     //Dobbelsteen code
+        {
+
+            int dice_1, dice_2;
+            string tekst = "";
+
+            Random rnd = new Random();
+
+            dice_1 = rnd.Next(1, 7);
+            dice_2 = rnd.Next(1, 7);
+
+            ///Dice_1
+
+            if (dice_1 == 1)                                            
+            {
+                pictureBox1.Image = Properties.Resources._1;
+            }
+
+            if (dice_1 == 2)
+            {
+                pictureBox1.Image = Properties.Resources._2;
+            }
+
+            if (dice_1 == 3)
+            {
+                pictureBox1.Image = Properties.Resources._3;
+            }
+
+            if (dice_1 == 4)
+            {
+                pictureBox1.Image = Properties.Resources._4;
+            }
+
+            if (dice_1 == 5)
+            {
+                pictureBox1.Image = Properties.Resources._5;
+            }
+
+            if (dice_1 == 6)
+            {
+                pictureBox1.Image = Properties.Resources._6;
+            }
+
+            ///Dice_2
+
+            if (dice_2 == 1)                                            
+            {
+                pictureBox2.Image = Properties.Resources._1;
+            }
+
+            if (dice_2 == 2)
+            {
+                pictureBox2.Image = Properties.Resources._2;
+            }
+
+            if (dice_2 == 3)
+            {
+                pictureBox2.Image = Properties.Resources._3;
+            }
+
+            if (dice_2 == 4)
+            {
+                pictureBox2.Image = Properties.Resources._4;
+            }
+
+            if (dice_2 == 5)
+            {
+                pictureBox2.Image = Properties.Resources._5;
+            }
+
+            if (dice_2 == 6)
+            {
+                pictureBox2.Image = Properties.Resources._6;
+            }
+        }
+
+    }
+}
