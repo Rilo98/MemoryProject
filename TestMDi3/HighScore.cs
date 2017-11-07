b9bdebdc2016d2fba4ed754af9017e5e1739e100
sapppie <32542173+sapppie@users.noreply.github.com>
2017-11-07 02:34:18 +0100

    overbodige code weggehaald uit highscore.cs

--- TestMDi3/HighScore.cs
+++ TestMDi3/HighScore.cs
@@ -20,52 +20,6 @@ namespace TestMDi3
 
             this.StartPosition = FormStartPosition.Manual;
             this.Location = new Point(0, 0);
-
-            if (File.Exists("HighscoreSP.sav"))
-            {
-                HighscoreName.Text = "";
-                Highscorecombo.Text = "";
-                Highscorescore.Text = "";
-
-                XmlDocument xml = new XmlDocument();
-                xml.Load("HighscoreSP.sav");
-                var name = "highscore/name";
-                var combo = "highscore/combo";
-                var score = "highscore/score";
-                var i = 0;
-                var j = 0;
-                var k = 0;
-                XmlNodeList names = xml.DocumentElement.SelectNodes(name);
-                foreach (XmlNode node in names)
-                {
-                    HighscoreName.Text += node.InnerXml + Environment.NewLine;
-                    i++;
-                    if (i >= 10) break;
-                }
-
-                XmlNodeList combos = xml.DocumentElement.SelectNodes(combo);
-                foreach (XmlNode node in combos)
-                {
-                    Highscorecombo.Text += node.InnerXml + Environment.NewLine;
-                    j++;
-                    if (j >= 10) break;
-                }
-
-                XmlNodeList scores = xml.DocumentElement.SelectNodes(score);
-                foreach (XmlNode node in scores)
-                {
-                    Highscorescore.Text += node.InnerXml + Environment.NewLine;
-                    k++;
-                    if (k >= 10) break;
-                }
-            }
-            else
-            {
-                HighscoreName.Text = "";
-                Highscorecombo.Text = "";
-                Highscorescore.Text = "";
-            }
-
         }
 
         private void button1_Click(object sender, EventArgs e)
